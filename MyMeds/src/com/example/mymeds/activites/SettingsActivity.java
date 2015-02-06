338715d98107c5416d740b4d004e06eba982823f
VChow <victorchow_507@hotmail.com>
2015-02-06 17:29:40 +0000

    Created layout xml file for Settings Activity. Updated Strings.xml for array values.

--- MyMeds/src/com/example/mymeds/activites/SettingsActivity.java
+++ MyMeds/src/com/example/mymeds/activites/SettingsActivity.java
@@ -1,7 +1,17 @@
 package com.example.mymeds.activites;
 
+import com.example.mymeds.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SettingsActivity extends Activity{
 
-}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_settings);
+		
+		//TODO - Implementation Code
+	}
+}   
