27d34989c0ed021d9e7b91ec0b0363a5ade66768
Guy Templeton <guyjtempleton@googlemail.com>
2015-02-09 17:19:42 +0000

    Creation of Date pickers for medication required
    Creates date picker on button presses for start and end dates

--- MyMeds/src/com/example/mymeds/fragments/ThirdFragment.java
+++ MyMeds/src/com/example/mymeds/fragments/ThirdFragment.java
@@ -1,7 +1,12 @@
 package com.example.mymeds.fragments;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import com.example.mymeds.R;
 
+import android.app.Activity;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,11 +17,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupWindow;
 
 public class ThirdFragment extends Fragment {
 
 	PopupWindow pw;
+	OnDateEntrySelectedListener mCallback;
+	long startDate, def = 0;
+	boolean startPressed =false;
+	EditText startDateText, endDateText;
+
+	public interface OnDateEntrySelectedListener {
+        public void onDateEntrySelected(boolean startDate, long defaultDate);
+    }
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,22 +38,61 @@ public class ThirdFragment extends Fragment {
 
 		View rootView = inflater.inflate(R.layout.fragment_third, container, false);
 
-		final Button acceptBtn = (Button) rootView.findViewById(R.id.accept);
+		final Button startBtn = (Button) rootView.findViewById(R.id.start);
+		final Button endBtn = (Button) rootView.findViewById(R.id.end);
+		startDateText = (EditText) rootView.findViewById(R.id.startDate);
+		endDateText = (EditText) rootView.findViewById(R.id.endDate);
 
-		acceptBtn.setOnClickListener(new OnClickListener() {
+		startBtn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				try {
-					initiatePopupWindow();
+					startPressed = true;
+					mCallback.onDateEntrySelected(true, System.currentTimeMillis());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 
 			}
-		});		
+		});
+		
+		endBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if(startDate==0){
+					//Have the default date as tomorrow if the start date hasn't been
+					//selected yet
+					def = System.currentTimeMillis() + 86400000;
+				}
+				else{
+					def = startDate + 86400000;
+				}
+				try {
+					startPressed = false;
+					mCallback.onDateEntrySelected(false, def);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 
 		return rootView;
 	}
+	
+	@Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnDateEntrySelectedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnHeadlineSelectedListener");
+        }
+    }
+
 
 
 	private void initiatePopupWindow() {
@@ -73,4 +126,18 @@ public class ThirdFragment extends Fragment {
 			pw.dismiss();
 		}
 	};
+	
+	public void setDate(long date){
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		Calendar c = Calendar.getInstance();
+		c.setTimeInMillis(date);
+		String text = formatter.format(c.getTime());
+		
+		if(startPressed){			
+			 startDateText.setText(text);
+		}
+		else{
+			endDateText.setText(text);
+		}
+	}
 }
