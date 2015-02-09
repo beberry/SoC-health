cbb75c64c114576b4b1cea904ef6ac5c149ef816
Johnathan Law <johnathanlaw@gmail.com>
2015-02-09 10:51:00 +0000

    Adds theme to manifest
    Adds theme to AndroidManifest.xml and adds
    requestWindowFeature(Window.FEATURE_ACTION_BAR) to MainActivity.java

--- MyMeds/src/com/example/mymeds/activites/MainActivity.java
+++ MyMeds/src/com/example/mymeds/activites/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 import com.example.mymeds.R;
 import com.example.mymeds.util.TabsPagerAdapter;
@@ -26,6 +27,7 @@ ActionBar.TabListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_ACTION_BAR);
 		setContentView(R.layout.activity_main);
 
 		// Initilization
