3c136afa58c6e8363281ad5c25190033e73b8b9e
Johnathan Law <johnathanlaw@gmail.com>
2015-02-16 18:51:49 +0000

    Revert 1b03701..6f4df42
    This rolls back to commit 1b037019d635b7b6ca8cdb7fce0d0f9956ab3d54.

--- MyMeds/src/com/example/mymeds/activites/SettingsActivity.java
+++ MyMeds/src/com/example/mymeds/activites/SettingsActivity.java
@@ -18,15 +18,11 @@ import com.example.mymeds.stores.SettingsStore;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
@@ -54,50 +50,6 @@ public class SettingsActivity extends Activity{
 		Spinner spinnerSnoozeTime = (Spinner) findViewById(R.id.spinnerSnoozeTime);
 		Spinner spinnerHowLongBefore = (Spinner) findViewById(R.id.spinnerHowLongBefore);
 		Spinner spinnerTextSize = (Spinner) findViewById(R.id.spinnerTextSize);
-		
-		spinnerSoundsSelection.setOnItemSelectedListener(new OnItemSelectedListener()
-		{
-			int count = 0;
-			@Override
-			public void onItemSelected(AdapterView<?> parent, View view,
-					int position, long id) {
-				Spinner sound_select = (Spinner) findViewById(R.id.spinnerSoundsSelection);
-				/*
-				 * 		<item>Alarm Sound</item>
-						<item>Ringtone Sound</item>
-						<item>Notification Sound</item>
-				 */
-				if(count > 0 )
-				{
-					if(sound_select.getSelectedItem().equals("Notification Sound"))
-					{
-						MediaPlayer player = MediaPlayer.create(getApplicationContext(),
-							    Settings.System.DEFAULT_NOTIFICATION_URI);
-							player.start();
-					}
-					else if(sound_select.getSelectedItem().equals("Ringtone Sound"))
-					{
-						MediaPlayer player = MediaPlayer.create(getApplicationContext(),
-							    Settings.System.DEFAULT_NOTIFICATION_URI);
-							player.start();
-					}
-					else if(sound_select.getSelectedItem().equals("Alarm Sound"))
-					{
-						MediaPlayer player = MediaPlayer.create(getApplicationContext(),
-							    Settings.System.DEFAULT_NOTIFICATION_URI);
-							player.start();
-					}
-				}
-				count++;
-			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> parent) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-		});
 		
 		
 		SettingsStore store = null;
