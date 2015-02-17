b3c231d53db42e78d82eb305c7d622eca6ad6b23
Guy Templeton <guyjtempleton@googlemail.com>
2015-02-17 15:48:57 +0000

    Changes to medicine taking logic
    Also cleanup of JSONUtils

--- MyMeds/src/com/example/mymeds/activites/MainActivity.java
+++ MyMeds/src/com/example/mymeds/activites/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mymeds.activites;
 
+import java.io.File;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -7,6 +8,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import android.app.TabActivity;
+import android.app.ActionBar.LayoutParams;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -18,11 +20,15 @@ import android.support.v4.view.MenuItemCompat;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewConfiguration;
 import android.widget.TabHost;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TabHost.TabSpec;
 
 import com.example.mymeds.R;
@@ -42,26 +48,29 @@ public class MainActivity extends TabActivity {
 	ArrayList<Medication> todaysMeds = new ArrayList<Medication>();
 	Context mContext;
 	MainActivity self = this;
+	MedFetcher mMedFetcher = new MedFetcher();
 
 	public void onCreate(Bundle savedInstanceState) {
+		File file = new File(this.getFilesDir()+"//"+"meddata.json");
+		if(!file.exists()){
 		JSONUtils.writeStringToFile(readAssets(), this.getApplicationContext(), true); // Forces overwrite of existing JSON.
+		}
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mContext=this;
-		
+
 		final Calendar c = Calendar.getInstance();
 		int year = c.get(Calendar.YEAR);
 		int month = c.get(Calendar.MONTH);
 		int day = c.get(Calendar.DAY_OF_MONTH);
-		
+
 		disableHardwareMenuKey();
 		gestureDetector = new GestureDetector(new SwipeGestureDetector());
-		MedFetcher mMedFetcher = new MedFetcher();
 		long today = MedFetcher.milliDate(year, month, day);
 
-		
+
 		allMeds = JSONUtils.loadValues(JSONUtils.readFile(this.getApplicationContext(), true), this.getApplicationContext());
-		
+
 		//Load all meds into mMedFetcher, get only today's, write to the today's meds file
 		mMedFetcher.loadAssets(this, allMeds);
 		JSONUtils.writeToFile(mMedFetcher.daysMedication(today), this, false); // Forces overwrite of existing JSON.
@@ -76,7 +85,7 @@ public class MainActivity extends TabActivity {
 				.setIndicator("Today's Medication", null)
 				.setContent(intentToday);
 
-		
+
 		Intent intentAll = new Intent().setClass(this, AllMeds.class);
 		intentAll.putParcelableArrayListExtra("meds", allMeds);
 		TabSpec tabSpecAll = tabHost
@@ -92,13 +101,13 @@ public class MainActivity extends TabActivity {
 				.setContent(intentFuture);
 
 		tabHost.setBackgroundResource(R.drawable.ab_stacked_solid_health);;
-		
+
 		// add all tabs 
 		tabHost.addTab(tabSpecToday);
 		tabHost.addTab(tabSpecAll);
 		tabHost.addTab(tabSpecProfile);
 		tabHost.setCurrentTab(0);
-		
+
 		Alarms alarm = new Alarms(getApplicationContext());
 		//alarm.setAllAlarms();
 		//alarm.addAlarm(0);
@@ -106,20 +115,31 @@ public class MainActivity extends TabActivity {
 		//alarm.addAlarm(2);
 		//alarm.setNextAlarm(0, 02300, "2300");
 		LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
-			      new IntentFilter("Med-Taken"));
+				new IntentFilter("Med-Taken"));
 	}
-	
+
 	// Our handler for received Intents. This will be called whenever an Intent
 	// with an action named "custom-event-name" is broadcasted.
 	private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
-	  @Override
-	  public void onReceive(Context context, Intent intent) {
-	    // Get extra data included in the Intent
-	    ArrayList<Medication> temp = new ArrayList<Medication>();
-		temp = intent.getParcelableArrayListExtra("medData");
-		allMeds = temp;
-		self.recreate();
-	  }
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// Get extra data included in the Intent
+			//Update the locally held todays meds, knock out the first frequency
+			int position = intent.getIntExtra("position", 0);
+
+			//Modify and update the numbers of pills remaining for this pill
+			allMeds.get(todaysMeds.get(position).getIndex()).setRemaining(allMeds.get(todaysMeds.get(position).getIndex()).getRemaining()-todaysMeds.get(position).getFrequency().get(0).getUnits());
+			if(todaysMeds.get(position).getFrequency().size()==1){
+				todaysMeds.remove(position);
+			}
+			else{
+				todaysMeds.get(position).getFrequency().remove(0);
+			}
+			
+			JSONUtils.writeToFile(todaysMeds, mContext, false);
+			//Force refresh of data
+			self.recreate();
+		}
 	};
 
 	public void onResume(Bundle savedInstanceState){
@@ -134,17 +154,17 @@ public class MainActivity extends TabActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-				//getMenuInflater().inflate(R.menu.main, menu);
+		//getMenuInflater().inflate(R.menu.main, menu);
 
-				MenuItem itemBlog = menu.add(Menu.NONE, // Group ID
-						R.id.action_settings, // Item ID
-						101, // Order
-						"Blog"); // Title
-				// To showAsAction attribute, use MenuItemCompat (set to always)
-				MenuItemCompat.setShowAsAction(itemBlog, MenuItem.SHOW_AS_ACTION_ALWAYS);
-				itemBlog.setIcon(R.drawable.ic_settings);
-				super.onCreateOptionsMenu(menu);
-				return true;
+		MenuItem itemBlog = menu.add(Menu.NONE, // Group ID
+				R.id.action_settings, // Item ID
+				101, // Order
+				"Blog"); // Title
+		// To showAsAction attribute, use MenuItemCompat (set to always)
+		MenuItemCompat.setShowAsAction(itemBlog, MenuItem.SHOW_AS_ACTION_ALWAYS);
+		itemBlog.setIcon(R.drawable.ic_settings);
+		super.onCreateOptionsMenu(menu);
+		return true;
 	} 
 
 	/**
@@ -171,7 +191,7 @@ public class MainActivity extends TabActivity {
 	protected String readAssets()
 	{	
 		String bufferString = null;
-		
+
 		try
 		{
 			AssetManager assetManager = getAssets();
@@ -186,10 +206,10 @@ public class MainActivity extends TabActivity {
 		{
 			e.printStackTrace();
 		}
-		
+
 		return bufferString;
 	}
-	
+
 	/**
 	 * Method to get today's date and then calculate medication required for today.
 	 */
@@ -200,7 +220,7 @@ public class MainActivity extends TabActivity {
 		Calendar c = new GregorianCalendar();
 		todaysMeds = medFetcher.daysMedication(c.getTime().getTime());
 	}
-	
+
 	/**
 	 * Disable Hardware Menu Button on phones. Force Menu drop down on Action Bar.
 	 * 
